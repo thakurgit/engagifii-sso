85c164cba3a04e37b960923908b63ff58b961096
prakash thakur <prakashthakur@crescerance.com>
2025-06-26 11:27:29 +0530

    version update 2.2.2

--- engagifii-sso.php
+++ engagifii-sso.php
@@ -5,7 +5,7 @@
  * Description: Enables SSO login with a Engagifii credentials.
  * Author:      Engagifii
  * Author URI:  https://Crescerance.com/
- * Version:     2.2.1
+ * Version:     2.2.2
  * Text Domain: engagifii-sso
  * Domain Path: /languages/
  * License:     GPLv3 or later (license.txt)
@@ -14,7 +14,7 @@
 if (!defined('ABSPATH')) {
     exit;
 }
- define('ENGAGIFII_SSO_VERSION','2.2.1');
+ define('ENGAGIFII_SSO_VERSION','2.2.2');
 // Add settings menu
 function engagifii_sso_menu() {
     add_menu_page(
